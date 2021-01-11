ba89c68f68ea3a207c40e015c09c7d3afbffa11e
Rick Porter <rick.e.porter@gmail.com>
2021-01-10 19:19:11 -0800

    cleaning up

--- packages/cdk/assembly/actor.ts
+++ packages/cdk/assembly/actor.ts
@@ -4,25 +4,21 @@ import { Principal } from "./candid/types/principal";
 class Actor {
     public owner: Principal;
     public time: i64;
-    //public storage: Storage;
 
     constructor(owner: Principal, time:i64){
         this.owner = owner;
         this.time = time;
-        //this.storage = new Storage();
         //TODO: set owner, tokens?, time?
     }
 
     //can be overrid
     preUpgrade() : void{
         API.print("[Counter] Canister pre upgrade called");
-        //this.storage.save();
     }
 
      //can be overrid
     postUpgrade() : void{
         API.print("[Counter] Canister post upgrade called");
-        //this.storage.reload();
     }
 }
 
